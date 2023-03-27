5ea00ca4e53e6069ba079b1c56298c0dcf90cc05
Curtis Xiao <xiao.curtis@gmail.com>
2023-03-26 22:03:18 -0600

    Make release easier

--- tools/build.bat
+++ tools/build.bat
@@ -4,30 +4,30 @@ mkdir dist
 ::windows x64
 set GOOS=windows
 set GOARCH=amd64
-go build -ldflags "-s -w" -o .\dist\DDatHome-windows-amd64.exe
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-windows-amd64.exe
 
 ::windows x32
 set GOOS=windows
 set GOARCH=386
-go build -ldflags "-s -w" -o .\dist\DDatHome-windows-386.exe
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-windows-386.exe
 
 ::linux x64
 set GOOS=linux
 set GOARCH=amd64
-go build -ldflags "-s -w" -o .\dist\DDatHome-linux-amd64
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-linux-amd64
 
 ::linux x32
 set GOOS=linux
 set GOARCH=386
-go build -ldflags "-s -w" -o .\dist\DDatHome-linux-386
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-linux-386
 
 ::linux arm7
 set GOOS=linux
 set GOARCH=arm
 set GOARM=7
-go build -ldflags "-s -w" -o .\dist\DDatHome-linux-arm7
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-linux-arm7
 
 ::linux arm64
 set GOOS=linux
 set GOARCH=arm64
-go build -ldflags "-s -w" -o .\dist\DDatHome-linux-arm64
+go build -ldflags "-s -w" -o .\dist\DDatHome-go-linux-arm64
